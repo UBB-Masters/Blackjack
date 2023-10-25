3bf1528c3635110470f5e0246c2b5d52a4c8d0a2
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2023-10-25 20:18:41 +0300

    Work on game logic in client

--- BlackjackClientPython/main.py
+++ BlackjackClientPython/main.py
@@ -16,5 +16,24 @@ response = client.recv(1024).decode()
 print("Received:", response)
 
 if response == "pong":
-    # Connection is established, proceed with further interactions
-    print("Connection established. Proceed with the interaction.")
+    # Connection is established, proceed with the interaction
+    print("Connection established. Proceed with the game.")
+
+    # Main game loop
+    while True:
+        # Receive and display the game state from the server
+        game_state = client.recv(1024).decode()
+        print("Game State:", game_state)
+
+        # Check if it's the player's turn (you can modify this based on your game's logic)
+        if "Your Turn" in game_state:
+            action = input("Enter your action (hit or stand): ")
+            client.send(action.encode())
+
+        # Check if the game is over
+        if "Game Over" in game_state:
+            print("Game Over. Thank you for playing!")
+            break
+
+    # Close the client socket when the game is finished
+    client.close()
