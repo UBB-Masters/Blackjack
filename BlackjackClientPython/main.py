4ba91cc7d18e8ef83d5d715ac3f1414233de7d66
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2023-10-25 20:07:42 +0300

    Connection is working

--- BlackjackClientPython/main.py
+++ BlackjackClientPython/main.py
@@ -8,12 +8,13 @@ server_port = 9999  # Use the same port as the server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((server_ip, server_port))
 
-# Implement the handshake mechanism
+# Send a single "ping" to the server to establish the connection
 client.send("ping".encode())
+
+# Receive and display the response (pong)
 response = client.recv(1024).decode()
+print("Received:", response)
 
 if response == "pong":
+    # Connection is established, proceed with further interactions
     print("Connection established. Proceed with the interaction.")
-else:
-    print("Connection failed.")
-    client.close()
--- BlackjackServer/main.py
+++ BlackjackServer/main.py
@@ -3,11 +3,11 @@ import socket
 # Initialize the server socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('0.0.0.0', 9999))
-server.listen(1)  # Listen for one client
+server.listen(2)  # Listen for two clients
 
-print("Server is waiting for a connection...")
+print("Server is waiting for player connections...")
 
-# Keep track of connected clients and their "ping" status
+# Keep track of connected players and their "ping" status
 connected_clients = {}
 
 while len(connected_clients) < 2:
@@ -22,4 +22,11 @@ while len(connected_clients) < 2:
         client.send("pong".encode())
         connected_clients[addr] = client
 
-# The server can now proceed with other interactions if needed
+# Both players are connected; the game can start
+print("Both players are connected. Starting the game...")
+
+# Implement the game logic here
+# You can add the Blackjack game logic and manage player interactions
+
+# Close the server when the game is finished
+server.close()
