74cce6fb6a9b5bf2f34cce7a705922d03e30b550
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2023-10-25 19:39:44 +0300

    Streamlined the code to ensure successful connection establishment

--- BlackjackClientPython/main.py
+++ BlackjackClientPython/main.py
@@ -1,23 +1,19 @@
 import socket
 
 # Define the server's IP address and port
-server_ip = 'YOUR_SERVER_IP'  # Replace with the actual server IP address
+server_ip = '10.152.0.236'  # Replace with the actual server IP address
 server_port = 9999  # Use the same port as the server
 
 # Create a socket to connect to the server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((server_ip, server_port))
 
-while True:
-    # Receive and display server messages
-    message = client.recv(1024).decode()
-    print(message)
+# Implement the handshake mechanism
+client.send("ping".encode())
+response = client.recv(1024).decode()
 
-    if 'Welcome' in message:
-        # Send player actions (hit, stand, double_down, or quit)
-        action = input("Enter your action (hit, stand, double_down, or quit): ")
-        client.send(action.encode())
-
-    elif 'Bust' in message or 'Stand' in message:
-        # Player turn is done; wait for the other player's turn
-        pass
+if response == "pong":
+    print("Connection established. Proceed with the interaction.")
+else:
+    print("Connection failed.")
+    client.close()
