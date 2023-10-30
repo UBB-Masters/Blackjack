0be5b52d733654a7b756dfe3caad8cb113ad1b08
LucaTheSorcerer <lucka.tudor@gmail.com>
2023-10-30 17:01:38 +0200

    might work

--- BlackjackClientPython/main.py
+++ BlackjackClientPython/main.py
@@ -1,24 +1,65 @@
+# import socket
+# import pickle
+# from BlackjackServer.BlackJack.black_jack_game import Blackjack, Player
+#
+# def game_client():
+#     server_ip = '127.0.0.1'
+#     server_port = 9999
+#
+#     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     client.connect((server_ip, server_port))
+#
+#     player_names = [input(f"Player {i}, say your name: ") for i in range(1, 3)]
+#     players = [Player(name) for name in player_names]
+#     game = Blackjack(players)
+#
+#     dealer_score, winning_players = game.play_game()
+#     game_result = (dealer_score, [player.name for player in winning_players])
+#
+#     client.send(pickle.dumps(game_result))  # Send game results to the server
+#     client.close()
+#
+# if __name__ == "__main__":
+#     game_client()
+#
 import socket
 import pickle
 from BlackjackServer.BlackJack.black_jack_game import Blackjack, Player
 
 def game_client():
-    server_ip = '127.0.0.1'
-    server_port = 9999
+    server_ip = '127.0.0.1'  # Replace with the IP address of the server
+    server_port = 9999  # The same port as the server
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((server_ip, server_port))
 
-    player_names = [input(f"Player {i}, say your name: ") for i in range(1, 3)]
-    players = [Player(name) for name in player_names]
-    game = Blackjack(players)
+    player_id = client.recv(1024).decode()  # Receive the assigned player ID
+    print(f"You are {player_id}")
 
-    dealer_score, winning_players = game.play_game()
-    game_result = (dealer_score, [player.name for player in winning_players])
+    if player_id == "Player 1":
+        # Initialize the game for Player 1
+        player_names = [input("Player 1, say your name: ")]
+        players = [Player(name) for name in player_names]
+        game = Blackjack(players)
 
-    client.send(pickle.dumps(game_result))  # Send game results to the server
-    client.close()
+        dealer_score, winning_players = game.play_game()
+        game_result = (dealer_score, [player.name for player in winning_players])
+
+        client.send(pickle.dumps(game_result))  # Send game results to the server
+        client.close()
+    elif player_id == "Player 2":
+        # Initialize the game for Player 2
+        player_names = [input("Player 2, say your name: ")]
+        players = [Player(name) for name in player_names]
+        game = Blackjack(players)
+
+        dealer_score, winning_players = game.play_game()
+        game_result = (dealer_score, [player.name for player in winning_players])
+
+        client.send(pickle.dumps(game_result))  # Send game results to the server
+        client.close()
+    else:
+        print("You are a spectator")
 
 if __name__ == "__main__":
     game_client()
-
--- BlackjackServer/main.py
+++ BlackjackServer/main.py
@@ -2,28 +2,67 @@ import socket
 import pickle
 import threading
 
+# def handle_client(client):
+#     data = client.recv(1024)
+#     try:
+#         game_result = pickle.loads(data)
+#         # Process the game result received from the client
+#         print("Dealer's Score:", game_result[0])
+#         print("Winning Players:", game_result[1])
+#     except pickle.UnpicklingError as e:
+#         print("Error receiving and processing the game result:", e)
+#
+#     client.close()
+#
+# def main():
+#     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server.bind(('0.0.0.0', 9999))
+#     server.listen(2)
+#
+#     print("Server is waiting for player connections...")
+#
+#     while True:
+#         client, addr = server.accept()
+#         print(f"Connected to {addr}")
+#
+#         client_thread = threading.Thread(target=handle_client, args=(client,))
+#         client_thread.start()
+#
+#     server.close()
+#
+# if __name__ == "__main__":
+#     main()
+import socket
+import pickle
+import threading
+
 def handle_client(client):
     data = client.recv(1024)
-    try:
-        game_result = pickle.loads(data)
-        # Process the game result received from the client
-        print("Dealer's Score:", game_result[0])
-        print("Winning Players:", game_result[1])
-    except pickle.UnpicklingError as e:
-        print("Error receiving and processing the game result:", e)
-
+    print(data.decode())  # Display the game result received from the client
     client.close()
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('0.0.0.0', 9999))
+    server.bind(('0.0.0.0', 9999))  # Server's IP address and the listening port
     server.listen(2)
 
     print("Server is waiting for player connections...")
 
+    player_count = 0  # To track the number of players
     while True:
         client, addr = server.accept()
-        print(f"Connected to {addr}")
+        player_count += 1
+
+        if player_count == 1:
+            player_id = "Player 1"
+        elif player_count == 2:
+            player_id = "Player 2"
+        else:
+            player_id = "Spectator"  # If more than two players connect
+
+        print(f"Connected to {player_id} at address {addr}")
+
+        client.send(player_id.encode())  # Sending the player ID to the client
 
         client_thread = threading.Thread(target=handle_client, args=(client,))
         client_thread.start()
