2b3a73b0ca2d54ee26e07f340069c7304fd8d9ab
LucaTheSorcerer <lucka.tudor@gmail.com>
2023-11-28 20:12:26 +0200

    Made some idk what modifications here

--- BlackjackServerJava/untitled/src/main/java/org/example/BlackjackServer.java
+++ BlackjackServerJava/untitled/src/main/java/org/example/BlackjackServer.java
@@ -1,3 +1,82 @@
+//package org.example;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.net.ServerSocket;
+//import java.net.Socket;
+//
+//public class BlackjackServer {
+//    private static final int MAX_PLAYERS = 2; // Maximum number of players
+//
+//    public static void main(String[] args) {
+//        int playerCount = 0;
+//        ServerSocket serverSocket = null;
+//        try {
+//            serverSocket = new ServerSocket(9999); // Listening port
+//            System.out.println("Server is waiting for player connections...");
+//
+//            while (playerCount < MAX_PLAYERS) {
+//                Socket client = serverSocket.accept();
+//                playerCount++;
+//
+//                String playerID = "Spectator"; // If more than two players connect
+//                if (playerCount == 1) {
+//                    playerID = "Player 1";
+//                } else if (playerCount == 2) {
+//                    playerID = "Player 2";
+//                }
+//
+//                System.out.println("Connected to " + playerID + " at address " + client.getRemoteSocketAddress());
+//
+//                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+//                String playerIDData = in.readLine(); // Read the player ID sent by the client
+//                String gameResultData = in.readLine(); // Read the game result sent by the client
+//
+//                System.out.println(playerIDData + ": " + gameResultData);
+//
+//                Thread clientThread = new Thread(new HandleClient(client, playerIDData, gameResultData));
+//                clientThread.start();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                if (serverSocket != null) {
+//                    serverSocket.close();
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//}
+//
+//class HandleClient implements Runnable {
+//    private final Socket client;
+//    private final String playerID;
+//    private final String gameResult;
+//
+//    public HandleClient(Socket client, String playerID, String gameResult) {
+//        this.client = client;
+//        this.playerID = playerID;
+//        this.gameResult = gameResult;
+//    }
+//
+//    @Override
+//    public void run() {
+//        try {
+//            System.out.println(playerID + ": " + gameResult);
+//
+//            client.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
+//
+
+
 package org.example;
 
 import java.io.BufferedReader;
@@ -10,32 +89,15 @@ public class BlackjackServer {
     private static final int MAX_PLAYERS = 2; // Maximum number of players
 
     public static void main(String[] args) {
-        int playerCount = 0;
         ServerSocket serverSocket = null;
         try {
             serverSocket = new ServerSocket(9999); // Listening port
             System.out.println("Server is waiting for player connections...");
 
-            while (playerCount < MAX_PLAYERS) {
+            while (true) {
                 Socket client = serverSocket.accept();
-                playerCount++;
 
-                String playerID = "Spectator"; // If more than two players connect
-                if (playerCount == 1) {
-                    playerID = "Player 1";
-                } else if (playerCount == 2) {
-                    playerID = "Player 2";
-                }
-
-                System.out.println("Connected to " + playerID + " at address " + client.getRemoteSocketAddress());
-
-                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                String playerIDData = in.readLine(); // Read the player ID sent by the client
-                String gameResultData = in.readLine(); // Read the game result sent by the client
-
-                System.out.println(playerIDData + ": " + gameResultData);
-
-                Thread clientThread = new Thread(new HandleClient(client, playerIDData, gameResultData));
+                Thread clientThread = new Thread(new HandleClient(client));
                 clientThread.start();
             }
         } catch (IOException e) {
@@ -54,19 +116,19 @@ public class BlackjackServer {
 
 class HandleClient implements Runnable {
     private final Socket client;
-    private final String playerID;
-    private final String gameResult;
 
-    public HandleClient(Socket client, String playerID, String gameResult) {
+    public HandleClient(Socket client) {
         this.client = client;
-        this.playerID = playerID;
-        this.gameResult = gameResult;
     }
 
     @Override
     public void run() {
         try {
-            System.out.println(playerID + ": " + gameResult);
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            String playerIDData = in.readLine(); // Read the player ID sent by the client
+            String gameResultData = in.readLine(); // Read the game result sent by the client
+
+            System.out.println(playerIDData + ": " + gameResultData);
 
             client.close();
         } catch (IOException e) {
